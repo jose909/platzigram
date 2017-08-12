f44e98006c7b36f2305585d502267674de75fbb7
jose909 <rosaleayal19@hotmail.com>
2017-08-12 12:13:40 -0600

    se agrego Page.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var page = require('page');
+
+var main = document.getElementById('main-container')
+
+page('/', function (ctx, next) {
+  main.innerHTML = 'Home';
+})
+
+page('/signup', function (ctx, next) {
+  main.innerHTML = 'Signup';
+})
+
+page()
