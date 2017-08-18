3b8cf4028192b92939d143d78e34283e5c8d3b8f
jose909 <rosaleayal19@hotmail.com>
2017-08-18 15:08:20 -0600

    se esta utilizando FormatJS, podemos disponer de traducción de fecha.

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,10 +1,44 @@
 var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
-var title  = require('title');
+var title = require('title');
 
 page('/', function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template);
+
+  var pictures = [
+    {
+      user: {
+        username: 'Jose Rosales',
+        avatar: 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/10171745_10152117693756888_1635202168_n.jpg?oh=887935b2d746373ec3df853ecf5e2c1b&oe=5A32016B'
+      },
+      url: 'office.jpg',
+      likes: 10,
+      liked: false,
+      createdAt: new Date()
+    },
+    {
+      user: {
+        username: 'Jose Rosales',
+        avatar: 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/10171745_10152117693756888_1635202168_n.jpg?oh=887935b2d746373ec3df853ecf5e2c1b&oe=5A32016B'
+      },
+      url: 'office.jpg',
+      likes: 2,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 10)
+    },
+    {
+      user: {
+        username: 'Jose Rosales',
+        avatar: 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/10171745_10152117693756888_1635202168_n.jpg?oh=887935b2d746373ec3df853ecf5e2c1b&oe=5A32016B'
+      },
+      url: 'office.jpg',
+      likes: 29,
+      liked: false,
+      createdAt: new Date().setDate(new Date().getDate() - 20)
+    }
+  ];
+
+  empty(main).appendChild(template(pictures));
 })
